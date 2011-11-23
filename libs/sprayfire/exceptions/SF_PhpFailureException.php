32bd676d52ef83caed60de84819a4286e777134b
Charles Sprayberry <cspray@gmail.com>
2011-11-23 11:46:36 -0500

    ADD the basic exceptions used by the framework

---
+++ libs/sprayfire/exceptions/SF_PhpFailureException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * SprayFire is a custom built framework intended to ease the development
+ * of websites with PHP 5.2.
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ *
+ * @author Charles Sprayberry <cspray at gmail dot com>
+ * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+/**
+ * An exception for when the PHP language constructs fail in a way that we can't
+ * account for.
+ */
+class SF_PhpFailureException extends Exception {
+
+}
+
+// End SF_PhpFailureException
---
+++ libs/sprayfire/exceptions/SF_UnknownClassException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * SprayFire is a custom built framework intended to ease the development
+ * of websites with PHP 5.2.
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ *
+ * @author Charles Sprayberry <cspray at gmail dot com>
+ * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+/**
+ * An exception thrown if the requested object could not be properly instantiated
+ * because the class could not be found.
+ */
+class SF_UnknownClassException extends Exception {
+
+}
+
+// End SF_UnknownClassException
