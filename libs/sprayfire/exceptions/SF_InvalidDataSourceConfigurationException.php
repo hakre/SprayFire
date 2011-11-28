bef5db81f7ceb0070da1e9a707426aedb2e4f7fb
Charles Sprayberry <cspray@gmail.com>
2011-11-27 20:40:40 -0500

    ADD exceptions for invalid data source objects and dependencies

---
+++ libs/sprayfire/exceptions/SF_InvalidDataSourceConfigurationException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * SprayFire is a custom built framework intended to ease the development
+ * of websites with PHP 5.2.
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ *
+ * @author Charles Sprayberry <cspray at gmail dot com>
+ * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+/**
+ * Thrown if the configuration object for the data source could not be instantiated
+ * in the framework creation of the data source object.
+ */
+class SF_InvalidDataSourceConfigurationException extends Exception {
+
+}
+
+// End SF_InvalidDataSourceConfigurationException
