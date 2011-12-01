630c70ca1d6227309cd5f8e5df11221cd29d8b68
Charles Sprayberry <cspray@gmail.com>
2011-12-01 13:33:42 -0500

    ADD an interface for objects that should be considered bootstrappers

---
+++ libs/sprayfire/interfaces/Bootstrapper.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * SprayFire is a custom built framework intended to ease the development
+ * of websites with PHP 5.2.
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ *
+ * @author Charles Sprayberry <cspray at gmail dot com>
+ * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+/**
+ * An interface implemented by all framework and app bootstrapping objects.
+ */
+interface Bootstrapper {
+
+    /**
+     * Every bootstrapper should be prepared to accept a configuration object, as
+     * many of the responsibilities for the bootstrapping object is to ensure
+     * configuration values are properly set.
+     *
+     * @param Configuration $ConfigurationObject
+     */
+    public function __construct(Configuration $ConfigurationObject);
+
+    /**
+     * A method that should do whatever bootstrapping features are needed for that
+     * particular bootstrap.
+     */
+    public function runBootstrap();
+
+}
+
+// End SF_IsBootstrapper
