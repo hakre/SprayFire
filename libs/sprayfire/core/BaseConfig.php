8ff5aecfb5679919da48c8fa692332543241945d
Charles Sprayberry <cspray@gmail.com>
2011-12-01 13:23:04 -0500

    ADD the framework's default configuration object

---
+++ libs/sprayfire/core/BaseConfig.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * SprayFire is a custom built framework intended to ease the development
+ * of websites with PHP 5.2.
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ *
+ * @author Charles Sprayberry <cspray at gmail dot com>
+ * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+/**
+ * A simple, generic configuration object to be extended by framework and application
+ * objects.
+ */
+abstract class BaseConfig extends CoreObject implements Configuration {
+
+    /**
+     * The data structure holding the key value pairs.
+     *
+     * @var array
+     */
+    private $dataStorage = array();
+
+    /**
+     * Nothing is happening in the constructor, feel free to override in children
+     * classes.
+     *
+     * It is advised that you add a call to parent::__construct() to ensure future
+     * compatibility if the constructor is changed.
+     */
+    public function __construct() {
+
+    }
+
+    /**
+     * @param string $key
+     * @return mixed|null
+     */
+    public function read($key) {
+        if (!isset($this->dataStorage[$key])) {
+            return NULL;
+        }
+        return $this->dataStorage[$key];
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return boolean
+     */
+    public function write($key, $value) {
+        $this->dataStorage[$key] = $value;
+        return isset($this->dataStorage[$key]);
+    }
+
+}
+
+// End BaseConfig
