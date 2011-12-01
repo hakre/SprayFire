58b1857e99dbcc84d42b5ddedb65c9c5b9515bb0
Charles Sprayberry <cspray@gmail.com>
2011-12-01 13:36:59 -0500

    ADD the file all requests are sent through

---
+++ web/index.php
@@ -0,0 +1,49 @@
+<?php
+
+   /**
+    * SprayFire is a custom built framework intended to ease the development
+    * of websites with PHP 5.2.
+    *
+    * SprayFire is released under the Open-Source Initiative MIT license.
+    *
+    * @author Charles Sprayberry <cspray at gmail dot com>
+    * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+    * @copyright Copyright (c) 2011, Charles Sprayberry
+    */
+
+   /**
+    * @var string
+    */
+    defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
+    /**
+     * @var string
+     */
+    defined('ROOT_PATH') or define('ROOT_PATH', dirname(dirname(__FILE__)));
+
+    /**
+     * @var string
+     */
+    defined('FRAMEWORK_PATH') or define('FRAMEWORK_PATH', ROOT_PATH . DS . 'libs' . DS . 'sprayfire');
+
+    /**
+     * @var string
+     */
+    defined('APP_PATH') or define('APP_PATH', ROOT_PATH . DS . 'app');
+
+    /**
+     * @var string
+     */
+    defined('FRAMEWORK_VERSION') or define('FRAMEWORK_VERSION', '0.10b');
+
+    include FRAMEWORK_PATH . DS . 'core' . DS . 'ClassLoader.php';
+
+    $ClassLoader = new ClassLoader();
+    $ClassLoader->setAutoloader();
+
+    $CoreConfiguration = new CoreConfiguration();
+    $FrameworkBootstrapper = new FrameworkBootstrap($CoreConfiguration);
+
+    $FrameworkBootstrapper->runBootstrap();
+
+    echo '<pre>', var_dump($CoreConfiguration), '</pre>';
