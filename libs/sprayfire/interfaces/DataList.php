a5a410c261a7eafe5d6d3289306914e828bab0ca
Charles Sprayberry <cspray@gmail.com>
2011-12-07 19:05:30 -0500

    UPD changing the type hint on parameters to the Object interface instead of the CoreObject implementation

--- libs/sprayfire/interfaces/DataList.php
+++ libs/sprayfire/interfaces/DataList.php
@@ -17,7 +17,7 @@
  * or class name set by the constructor.
  *
  * Only one data type may be stored in a list at one time.  We have chose to force
- * the passing of CoreObject instead of stdClass to ensure that the objects have
+ * the passing of Object instead of stdClass to ensure that the objects have
  * some kind of `equals()` method to invoke.
  */
 interface DataList extends Iterator {
@@ -34,15 +34,15 @@ interface DataList extends Iterator {
      * Should throw an exception if the list implementation does not have the proper
      * number of buckets available or the object passed is not of the correct type.
      *
-     * @param CoreObject $Object
+     * @param Object $Object
      * @return void
      * @throws InvalidArgumentException
      *         OutOfRangeException
      */
-    public function add(CoreObject $Object);
+    public function add(Object $Object);
 
     /**
-     * Will return the CoreObject that is found at the index passed, if the index
+     * Will return the Object that is found at the index passed, if the index
      * is invalid an OutOfRangeException should be thrown.
      *
      * Should return an instance of an object that implements the parent type passed
@@ -63,24 +63,24 @@ interface DataList extends Iterator {
      * operation it should throw an UnsupportedOperationException.
      *
      * @param int $index
-     * @param CoreObject $Object
+     * @param Object $Object
      * @return boolean
      * @throws OutOfRangeException
      *         InvalidArgumentException
      *         UnsupportedOperationException
      *
      */
-    public function set($index, CoreObject $Object);
+    public function set($index, Object $Object);
 
     /**
      * Will verify that the given object exists in the list of objects, the `equals()`
      * method should be invoked on the objects stored to determine if they are
      * equal to one another.
      *
-     * @param CoreObject $Object
+     * @param Object $Object
      * @return boolean
      */
-    public function contains(CoreObject $Object);
+    public function contains(Object $Object);
 
     /**
      * Will remove the given object if it exists in the list.
@@ -88,21 +88,21 @@ interface DataList extends Iterator {
      * This method is optional, if the list implementation does not support this
      * operation it should throw an UnsupportedOperationException.
      *
-     * @param CoreObject $Object
-     * @return void
+     * @param Object $Object
+     * @return boolean
      * @throws InvalidArgumentException
      *         UnsupportedOperationException
      */
-    public function remove(CoreObject $Object);
+    public function remove(Object $Object);
 
     /**
      * Will return an integer value representing the index for the given object,
-     * if the object could not be found -1 will be returned.
+     * if the object could not be found false will be returned.
      *
      * @param CoreObject $Object
-     * @return int
+     * @return mixed
      */
-    public function indexOf(CoreObject $Object);
+    public function indexOf(Object $Object);
 
     /**
      * Will return true if the given list has no object stored in it.
