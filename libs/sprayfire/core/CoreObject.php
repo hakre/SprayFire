b5b310ea44db338395cfd4a99ef2013f50f7a069
Charles Sprayberry <cspray@gmail.com>
2011-12-08 19:31:31 -0500

    UPD adding namespaces to CoreObject

--- libs/sprayfire/core/CoreObject.php
+++ libs/sprayfire/core/CoreObject.php
@@ -2,20 +2,30 @@
 
 /**
  * SprayFire is a custom built framework intended to ease the development
- * of websites with PHP 5.2.
+ * of websites with PHP 5.3.
+ *
+ * SprayFire makes use of namespaces, a custom-built ORM layer, a completely
+ * object oriented approach and minimal invasiveness so you can make the framework
+ * do what YOU want to do.  Some things we take seriously over here at SprayFire
+ * includes clean, readable source, completely unit tested implementations and
+ * not polluting the global scope.
  *
  * SprayFire is released under the Open-Source Initiative MIT license.
  *
  * @author Charles Sprayberry <cspray at gmail dot com>
  * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
  * @copyright Copyright (c) 2011, Charles Sprayberry
+ * @version 0.10b
+ * @since 0.10b
  */
+
+namespace libs\sprayfire\core;
 
 /**
  * The base object for all application classes, allows for the overloading of
  * properties.
  */
-abstract class CoreObject implements Object {
+abstract class CoreObject implements \libs\sprayfire\interfaces\Object {
 
     /**
      * Returns a unique identifier for the calling object.
@@ -37,7 +47,7 @@ abstract class CoreObject implements Object {
      * @param Object $CompareObject
      * @return boolean
      */
-    public function equals(Object $CompareObject) {
+    public function equals(\libs\sprayfire\interfaces\Object $CompareObject) {
         $thisHash = $this->hashCode();
         $compareHash = $CompareObject->hashCode();
         $areHashesEqual = $thisHash === $compareHash;
