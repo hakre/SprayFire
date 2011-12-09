13e3fddeabb67ec0d46c97da3934ef7bc800f2ff
Charles Sprayberry <cspray@gmail.com>
2011-12-08 19:33:10 -0500

    UPD adding namespace and updating class-level phpdoc

--- libs/sprayfire/interfaces/Configuration.php
+++ libs/sprayfire/interfaces/Configuration.php
@@ -2,14 +2,24 @@
 
 /**
  * SprayFire is a custom built framework intended to ease the development
- * of websites with PHP 5.2.
+ * of websites with PHP 5.3.
+ *
+ * SprayFire makes use of namespaces, a custom-built ORM layer, a completely
+ * object oriented approach and minimal invasiveness so you can make the framework
+ * do what YOU want to do.  Some things we take seriously over here at SprayFire
+ * includes clean, readable source, completely unit tested implementations and
+ * not polluting the global scope.
  *
  * SprayFire is released under the Open-Source Initiative MIT license.
  *
  * @author Charles Sprayberry <cspray at gmail dot com>
  * @license OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
  * @copyright Copyright (c) 2011, Charles Sprayberry
+ * @version 0.10b
+ * @since 0.10b
  */
+
+namespace libs\sprayfire\interfaces;
 
 /**
  * An interface for configuration objects used by the application and framework.
