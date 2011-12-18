56e2db22b2dcddab6c08145a8899eb5411e2fa48
Charles Sprayberry <cspray@gmail.com>
2011-12-18 15:49:38 -0500

    UPD adding some documentation to the Router interface

--- libs/sprayfire/request/Router.php
+++ libs/sprayfire/request/Router.php
@@ -23,16 +23,22 @@ namespace libs\sprayfire\request;
  * An interface for an object that will interpret a URI and map the pattern to a
  * Controller::action() as defined in the \libs\sprayfire\config\Configuration
  * object passed to it.
+ *
+ * @see https://github.com/cspray/SprayFire/wiki/Routing
  */
 interface Router {
 
     /**
+     * Should use the Configuration object passed to gather whatever details are
+     * necessary for the routing implementation to map a Uri object to a RoutedUri
+     * object.
      *
      * @param \libs\sprayfire\config\Configuration $RoutesConfig
      */
     public function __construct(\libs\sprayfire\config\Configuration $RoutesConfig);
 
     /**
+     * Should return a RoutedUri that is mapped off of the Uri object being passed.
      *
      * @param \libs\sprayfire\request\Uri $Uri
      * @return \libs\sprayfire\request\RoutedUri
