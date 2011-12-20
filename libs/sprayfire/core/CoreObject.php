988ffefcd7d6e12f451fad216cd88580b7fc103e
Charles Sprayberry <cspray@gmail.com>
2011-12-19 23:05:37 -0500

    UPD adding documentation to CoreObject

--- libs/sprayfire/core/CoreObject.php
+++ libs/sprayfire/core/CoreObject.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * @brief
+ * @brief The framework's implementation of the libs.sprayfire.core.Object interface.
  *
  * @details
  * SprayFire is a custom built framework intended to ease the development
@@ -20,49 +20,71 @@
  * @copyright Copyright (c) 2011, Charles Sprayberry
  */
 
-namespace libs\sprayfire\core;
-
 /**
- * The base object for all application classes; provides easy access to implementation
- * of the libs.sprayfire.core.Object interface.
+ * @namespace libs.sprayfire.core
+ * @brief Holds parts of the framework that are considered essential for SprayFire
+ * to operate.
  *
+ * @details
+ * The classes and interfaces in this namespace are generally things that work
+ * with the file system, load classes and otherwise make sure the framework has
+ * a common foundation to work with.  Ultimately you can almost think of this as
+ * a "utility" namespace, but ultimately goes into the heart of the framework.
  */
-abstract class CoreObject implements \libs\sprayfire\core\Object {
+namespace libs\sprayfire\core {
 
     /**
-     * Returns a unique identifier for the calling object.
+     * @brief SprayFire's implementation of the libs.sprayfire.core.Object interface,
+     * allowing easy access to some of the functionality required by this interface
+     * simply by extending CoreObject.
      *
-     * @return string
+     * @details
+     * Virtually all of the framework objects used by your application will extend
+     * this class.  So, if you use the framework as its intended to be used this
+     * functionality should be provided to you "out-of-the-box".  When you start
+     * creating your own objects be sure they somehow trace back to this object
+     * or an implementation of libs.sprayfire.core.Object or unexpected consequences
+     * may occur.
      */
-    public final function hashCode() {
-        $hashCode = spl_object_hash($this);
-        return $hashCode;
+    abstract class CoreObject implements \libs\sprayfire\core\Object {
+
+        /**
+         * @return A unique identifying string based on the internal memory pointer
+         * @see http://us3.php.net/manual/en/function.spl-object-hash.php
+         */
+        public final function hashCode() {
+            $hashCode = spl_object_hash($this);
+            return $hashCode;
+        }
+
+        /**
+         * @brief Default implementation, compares the libs.sprayfire.core.CoreObject::hashCode()
+         * return value to the passed \a $CompareObject.
+         *
+         * @details
+         * If your objects need to implement a Comparator be sure to override the
+         * implementation of this class.
+         *
+         * @param $CompareObject A libs.sprayfire.core.Object to compare to this one for equality
+         * @return True if the calling object and \a $CompareObject are equal, false if not
+         */
+        public function equals(\libs\sprayfire\core\Object $CompareObject) {
+            $thisHash = $this->hashCode();
+            $compareHash = $CompareObject->hashCode();
+            $areHashesEqual = $thisHash === $compareHash;
+            return $areHashesEqual;
+        }
+
+        /**
+         * @return A string representation of the calling object
+         */
+        public function __toString() {
+            return \get_class($this);
+        }
+
     }
 
-    /**
-     * The default implemenation for the equals function, will compare the hash
-     * code of the object passed and the calling object.
-     *
-     * If your objects need to implement a Comparator be sure to override the
-     * implementation of this class.
-     *
-     * @param $CompareObject A libs.sprayfire.core.Object to compare to this one for equality
-     * @return boolean
-     */
-    public function equals(\libs\sprayfire\core\Object $CompareObject) {
-        $thisHash = $this->hashCode();
-        $compareHash = $CompareObject->hashCode();
-        $areHashesEqual = $thisHash === $compareHash;
-        return $areHashesEqual;
-    }
-
-    /**
-     * @return string
-     */
-    public function __toString() {
-        return \get_class($this);
-    }
-
+    // End CoreObject
 }
 
-// End CoreObject
+// End libs.sprayfire.core
