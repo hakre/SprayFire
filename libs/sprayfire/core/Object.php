fdca519027c78c4859822826b5a6c0fd856a00a7
Charles Sprayberry <cspray@gmail.com>
2011-12-19 22:49:10 -0500

    UPD adding documentation to the Object interface

--- libs/sprayfire/core/Object.php
+++ libs/sprayfire/core/Object.php
@@ -2,7 +2,8 @@
 
 /**
  * @file
- * @brief
+ * @brief An interface representing a basic framework object, used as a means to
+ * ensure some very basic, common object functionality.
  *
  * @details
  * SprayFire is a custom built framework intended to ease the development
@@ -20,37 +21,55 @@
  * @copyright Copyright (c) 2011, Charles Sprayberry OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
 
-namespace libs\sprayfire\core;
-
 /**
- * The base interface all objects in the framework must implement; simply provides
- * a way to compare 2 objects, distinguish unique objects and return a textual
- * representation of the class.
+ * @namespace libs.sprayfire.core
+ * @brief Holds parts of the framework that are considered essential for SprayFire
+ * to operate.
+ *
+ * @details
+ * The classes and interfaces in this namespace are generally things that work
+ * with the file system, load classes and otherwise make sure the framework has
+ * a common foundation to work with.  Ultimately you can almost think of this as
+ * a "utility" namespace, but ultimately goes into the heart of the framework.
  */
-interface Object {
+namespace libs\sprayfire\core {
 
     /**
-     * Should check if the 2 objects are equal to one another, feel free to override
-     * this method, simply ensure that it returns true if object passed is equal
-     * to the calling object and false if it is not.
+     * @brief An interface for basic framework objects.
      *
-     * @param $Object libs.sprayfire.core.Object
-     * @return boolean
-     */
-    public function equals(\libs\sprayfire\core\Object $Object);
-
-    /**
-     * Should return a unique identifier for the calling object.
+     * @details
+     * Primarily this interface is used to ensure that there is a way to
      *
-     * @return string
+     * - represent a class as a string, for debugging purposes if necessary or
+     * for pure data objects and easy output
+     * - compare 2 objects to see if they are equal to each other based on a means
+     * that makes sense for the given object
+     * - return a unique identifier for the given object
      */
-    public function hashCode();
+    interface Object {
 
-    /**
-     * @return string
-     */
-    public function __toString();
+        /**
+         * @brief Compare two objects to see if they are equal to each other.
+         *
+         * @param $Object libs.sprayfire.core.Object
+         * @return boolean
+         */
+        public function equals(\libs\sprayfire\core\Object $Object);
+
+        /**
+         * @return A unique identifier for the given object
+         */
+        public function hashCode();
+
+        /**
+         * @return A string representation for the object
+         */
+        public function __toString();
+
+    }
+
+    // End Object
 
 }
 
-// End Object
+// End libs.sprayfire.core.Object
