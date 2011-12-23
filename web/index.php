71479728ca49a972e2f9b7ed7b7e535453bf9024
Charles Sprayberry <cspray@gmail.com>
2011-12-23 11:14:16 -0500

    UPD baby step toward making the initialization process more modular, allowing implementation details to be abstracted away

--- web/index.php
+++ web/index.php
@@ -15,14 +15,39 @@
         libs\sprayfire\core\ClassLoader;
 
    /**
-    * @var string
+    * @var DS A short-alias to DIRECTORY_SEPARATOR
     */
     defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
     /**
-     * @var string
+     * @var ROOT_PATH The appliaction's installation path
      */
     defined('ROOT_PATH') or define('ROOT_PATH', dirname(__DIR__));
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // The below variables can be changed and manipulated to adjust the implementation
+    // details of the framework's initialization process.
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    /**
+     * @var $ClassLoaderName The completely namespaced class that should be used
+     * as the primary framework autoloader.
+     */
+    $ClassLoaderName = '\\libs\\sprayfire\\core\\ClassLoader';
+
+    /**
+     * @var $primaryConfigPath An array representing a sub directory list to the
+     *      primary configuration file.
+     */
+    $primaryConfigPath = array('config', 'json', 'configuration.json');
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Ends the variables that allow for the changing of SprayFire implementation
+    //
+    // PLEASE DO NOT ALTER CODE BELOW THIS LINE!  DO SO AT YOUR OWN RISK!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
     include ROOT_PATH . DS . 'libs' . DS . 'sprayfire' . DS . 'core' . DS . 'SprayFireDirectory.php';
 
@@ -32,7 +57,7 @@
     include SprayFireDirectory::getFrameworkPathSubDirectory('core', 'CoreObject.php');
     include SprayFireDirectory::getFrameworkPathSubDirectory('core', 'ClassLoader.php');
 
-    $ClassLoader = new ClassLoader();
+    $ClassLoader = new $ClassLoaderName();
     $ClassLoader->setAutoloader();
 
     $primaryConfigPath = SprayFireDirectory::getAppPathSubDirectory('config', 'json', 'configuration.json');
