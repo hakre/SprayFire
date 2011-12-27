5a5536edede4869b12643b46547c968d85d6c3ae
Charles Sprayberry <cspray@gmail.com>
2011-12-27 15:51:35 -0500

    UPD fixing the tests for Directory

--- tests/DirectoryTest.php
+++ tests/DirectoryTest.php
@@ -44,10 +44,12 @@ class SprayFireDirectoryTest extends PHPUnit_Framework_TestCase {
         $this->installPath = \dirname(__DIR__);
         $libsPath = $this->installPath . '/tests/mockframework/libs';
         $appPath = $this->installPath . '/tests/mockframework/app';
+        $logsPath = $this->installPath . '/tests/mockframework/logs';
 
         \SprayFire\Core\Directory::setInstallPath($this->installPath);
         \SprayFire\Core\Directory::setLibsPath($libsPath);
         \SprayFire\Core\Directory::setAppPath($appPath);
+        \SprayFire\Core\Directory::setLogsPath($logsPath);
 
         $expectedAppSub = $this->installPath . '/tests/mockframework/app/TestApp/config';
         $actualAppSub = \SprayFire\Core\Directory::getAppPath('TestApp', 'config');
@@ -80,102 +82,75 @@ class SprayFireDirectoryTest extends PHPUnit_Framework_TestCase {
         $this->assertSame($expected, $actual);
     }
 
-//    public function testFrameworkSubDirectoryArrayOfArgumentsWithFileAtEnd() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'libs' . DS . 'sprayfire' . DS . 'config' . DS . 'json' . DS . 'configuration-test.json';
-//        $subDir = array('config', 'json', 'configuration-test.json');
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getFrameworkPathSubDirectory($subDir);
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testFrameworkSubDirectoryWithNoArguments() {
-//        $expected = libs\sprayfire\core\SprayFireDirectory::getFrameworkPath();
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getFrameworkPathSubDirectory();
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testLogsRootDirectory() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'logs';
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getLogsPath();
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testLogsSubDirectoryPassingListOfArguments() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'logs' . DS . 'error' . DS . 'database';
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory('error', 'database');
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testLogsSubDirectoryListOfArgumentsWithFileAtEnd() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'logs' . DS . 'error' . DS . 'database' . DS . 'sql-queries.txt';
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory('error', 'database', 'sql-queries.txt');
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testLogsSubDirectoryPassingArrayOfArguments() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'logs' . DS . 'error' . DS . 'database';
-//        $subDir = array('error', 'database');
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory($subDir);
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testLogsSubDirectoryArrayOfArgumentsWithFileAtEnd() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'logs' . DS . 'error' . DS . 'database' . DS . 'sql-queries.txt';
-//        $subDir = array('error', 'database', 'sql-queries.txt');
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory($subDir);
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testLogsSubDirectoryWithNoArguments() {
-//        $expected = libs\sprayfire\core\SprayFireDirectory::getLogsPath();
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory();
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testAppPathRootDirectory() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'app';
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getAppPath();
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testAppPathSubDirectoryListOfArguments() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'app' . DS . 'model' . DS . 'behaviors';
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory('model', 'behaviors');
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testAppSubDirectoryListOfArgumentsWithFileAtEnd() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'app' . DS . 'config' . DS . 'xml' . DS . 'configuration-test.xml';
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory('config', 'xml', 'configuration-test.xml');
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testAppPathSubDirectoryArrayOfArguments() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'app' . DS . 'model' . DS . 'behaviors';
-//        $subDir = array('model', 'behaviors');
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory($subDir);
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testAppSubDirectoryArrayOfArgumentsWithFileAtEnd() {
-//        $expected = ROOT_PATH . DS . 'tests' . DS . 'mockframework' . DS . 'app' . DS . 'config' . DS . 'xml' . DS . 'configuration-test.xml';
-//        $subDir = array('config', 'xml', 'configuration-test.xml');
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory($subDir);
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function testAppSubDirectoryWithNoArguments() {
-//        $expected = libs\sprayfire\core\SprayFireDirectory::getAppPath();
-//        $actual = libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory();
-//        $this->assertSame($expected, $actual);
-//    }
-//
-//    public function tearDown() {
-//        \libs\sprayfire\core\SprayFireDirectory::setRootInstallationPath(ROOT_PATH);
-//
-//        $this->assertSame($this->originalFrameworkPath, \libs\sprayfire\core\SprayFireDirectory::getFrameworkPathSubDirectory());
-//        $this->assertSame($this->originalAppPath, \libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory());
-//    }
+    public function testLibsPathPassingArrayOfArgumentsWithFileAtEnd() {
+        $expected = $this->installPath . '/tests/mockframework/libs/SprayFire/Config/json/configuration-test.json';
+        $subDir = array('SprayFire', 'Config', 'json', 'configuration-test.json');
+        $actual = \SprayFire\Core\Directory::getLibsPath($subDir);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testLogsPath() {
+        $expected = $this->installPath . '/tests/mockframework/logs';
+        $actual = \SprayFire\Core\Directory::getLogsPath();
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testLogsPathPassingListOfArguments() {
+        $expected = $this->installPath . '/tests/mockframework/logs/error/database';
+        $actual = \SprayFire\Core\Directory::getLogsPath('error', 'database');
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testLogsPathPassingListOfArgumentsWithFileAtEnd() {
+        $expected = $this->installPath . '/tests/mockframework/logs/error/database/sql-queries.txt';
+        $actual = \SprayFire\Core\Directory::getLogsPath('error', 'database', 'sql-queries.txt');
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testLogsPathPassingArrayOfArguments() {
+        $expected = $this->installPath . '/tests/mockframework/logs/error/database';
+        $subDir = array('error', 'database');
+        $actual = \SprayFire\Core\Directory::getLogsPath($subDir);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testLogsPathPassingArrayOfArgumentsWithFileAtEnd() {
+        $expected = $this->installPath . '/tests/mockframework/logs/error/database/sql-queries.txt';
+        $subDir = array('error', 'database', 'sql-queries.txt');
+        $actual = \SprayFire\Core\Directory::getLogsPath($subDir);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testAppPathRootDirectory() {
+        $expected = $this->installPath . '/tests/mockframework/app';
+        $actual = \SprayFire\Core\Directory::getAppPath();
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testAppPathSubDirectoryListOfArguments() {
+        $expected = $this->installPath . '/tests/mockframework/app/Model/Behavior';
+        $actual = \SprayFire\Core\Directory::getAppPath('Model', 'Behavior');
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testAppSubDirectoryListOfArgumentsWithFileAtEnd() {
+        $expected = $this->installPath . '/tests/mockframework/app/Config/xml/configuration-test.xml';
+        $actual = \SprayFire\Core\Directory::getAppPath('Config', 'xml', 'configuration-test.xml');
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testAppPathSubDirectoryArrayOfArguments() {
+        $expected = $this->installPath . '/tests/mockframework/app/Model/Behavior';
+        $subDir = array('Model', 'Behavior');
+        $actual = \SprayFire\Core\Directory::getAppPath($subDir);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testAppSubDirectoryArrayOfArgumentsWithFileAtEnd() {
+        $expected = $this->installPath . '/tests/mockframework/app/config/xml/configuration-test.xml';
+        $subDir = array('config', 'xml', 'configuration-test.xml');
+        $actual = \SprayFire\Core\Directory::getAppPath($subDir);
+        $this->assertSame($expected, $actual);
+    }
 
 }
-
-// End SprayFireDirectoryTest
