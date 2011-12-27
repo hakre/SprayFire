6c208fbf38fef38bc9c5fea55151e27f7ecfe89d
Charles Sprayberry <cspray@gmail.com>
2011-12-26 20:12:09 -0500

    ADD framework implementation for Logger and unit tests covering it

---
+++ libs/sprayfire/logger/FileLogger.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds an abstract class that allows
+ *
+ * @details
+ * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
+ * want to make simple, secure, dynamic website content.
+ *
+ * SprayFire repository: http://www.github.com/cspray/SprayFire/
+ *
+ * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
+ *
+ * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ *
+ * @author Charles Sprayberry cspray at gmail dot com
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+namespace libs\sprayfire\logger;
+use \SplFileInfo as SplFileInfo;
+use \InvalidArgumentException as InvalidArgumentException;
+use libs\sprayfire\logger\Logger as Logger;
+
+    /**
+     * @brief A framework implemented class that adds a timestamp log message to
+     * the end of an injected file.
+     */
+    class FileLogger implements Logger  {
+
+        /**
+         * @brief A SplFileObject that should be used to write log messages to.
+         *
+         * @property $LogFile
+         */
+        protected $LogFile;
+
+        /**
+         * @param $LogFile SplFileObject that should have log messages written to
+         */
+        public function __construct(SplFileInfo $LogFile) {
+            try {
+                $this->LogFile = $LogFile->openFile('a');
+            } catch (\RuntimeException $InvalidArgumentException) {
+                throw new InvalidArgumentException('There was an error attempting to open a writable log file.', null, $InvalidArgumentException);
+            }
+        }
+
+        /**
+         * @param $timestamp A formatted timestamp string
+         * @param $message The message string to log
+         * @return int The number of bytes written or null on error
+         */
+        public function log($timestamp, $message) {
+            if (!isset($timestamp) || empty($timestamp)) {
+                $timestamp = '00-00-0000 00:00:00';
+            }
+
+            if (!isset($message) || empty($message)) {
+                $message = 'Blank message.';
+            }
+
+            $separator = ' := ';
+            $message = $timestamp . $separator . $message . PHP_EOL;
+            return $this->LogFile->fwrite($message);
+        }
+
+    }
---
+++ tests/FileLoggerTest.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ *
+ * @details
+ * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
+ * want to make simple, secure, dynamic website content.
+ *
+ * SprayFire repository: http://www.github.com/cspray/SprayFire/
+ *
+ * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
+ *
+ * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ *
+ * @author Charles Sprayberry cspray at gmail dot com
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+/**
+ * @brief
+ */
+class FileLoggerTest extends PHPUnit_Framework_TestCase {
+
+    private $originalFrameworkPath;
+
+    private $originalAppPath;
+
+    private $readOnlyLog;
+
+    private $writableLog;
+
+    private $noTimestampLog;
+
+    public function setUp() {
+        $this->originalFrameworkPath = \libs\sprayfire\core\SprayFireDirectory::getFrameworkPathSubDirectory();
+        $this->originalAppPath = \libs\sprayfire\core\SprayFireDirectory::getAppPathSubDirectory();
+
+        $newRootPath = ROOT_PATH . DS . 'tests' . DS . 'mockframework';
+        \libs\sprayfire\core\SprayFireDirectory::setRootInstallationPath($newRootPath);
+
+        $this->assertSame($newRootPath . DS . 'libs' . DS . 'sprayfire', libs\sprayfire\core\SprayFireDirectory::getFrameworkPath());
+
+        $this->readOnlyLog = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory('readonly-log.txt');
+        $this->writableLog = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory('writable-log.txt');
+        $this->noTimestampLog = libs\sprayfire\core\SprayFireDirectory::getLogsPathSubDirectory('no-timestamp-log.txt');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testReadOnlyFileObjectFailure() {
+        $file = $this->readOnlyLog;
+        \touch($file);
+        \chmod($file, 0444);
+        $LogFile = new \SplFileInfo($file);
+        $Logger = new \libs\sprayfire\logger\FileLogger($LogFile);
+    }
+
+    public function testBasicFileLogging() {
+        $file = $this->writableLog;
+        \touch($file);
+        \chmod($file, 0755);
+        $LogFile = new \SplFileInfo($file);
+        $Logger = new libs\sprayfire\logger\FileLogger($LogFile);
+
+        $Logger->log('test','something');
+        $Logger->log('something', 'else');
+
+        $Log = $LogFile->openFile('r');
+        $i = 0;
+        $expected = array('test := something', 'something := else', '');
+        while (!$Log->eof()) {
+            $line = $Log->fgets();
+            $this->assertSame(\trim($line), $expected[$i]);
+            $i++;
+        }
+    }
+
+    public function testEmptyLogTimestamp() {
+        $file = $this->noTimestampLog;
+        \touch($file);
+        \chmod($file, 0755);
+        $LogFile = new \SplFileInfo($file);
+        $Logger = new libs\sprayfire\logger\FileLogger($LogFile);
+        $blankTimestamp = '';
+        $Logger->log($blankTimestamp,'something');
+        $Logger->log($blankTimestamp, 'else');
+
+        $Log = $LogFile->openFile('r');
+        $i = 0;
+        $expected = array('00-00-0000 00:00:00 := something', '00-00-0000 00:00:00 := else', '');
+        while (!$Log->eof()) {
+            $line = $Log->fgets();
+            $this->assertSame(\trim($line), $expected[$i]);
+            $i++;
+        }
+    }
+
+    public function testEmptyLogMessage() {
+        $file = $this->noTimestampLog;
+        \touch($file);
+        \chmod($file, 0755);
+        $LogFile = new \SplFileInfo($file);
+        $Logger = new libs\sprayfire\logger\FileLogger($LogFile);
+        $blankMessage = '';
+        $Logger->log('12-24-2011 12:45:12', $blankMessage);
+        $Logger->log('12-25-2011 13:56:10', $blankMessage);
+
+        $Log = $LogFile->openFile('r');
+        $i = 0;
+        $expected = array('12-24-2011 12:45:12 := Blank message.', '12-25-2011 13:56:10 := Blank message.', '');
+        while (!$Log->eof()) {
+            $line = $Log->fgets();
+            $this->assertSame(\trim($line), $expected[$i]);
+            $i++;
+        }
+    }
+
+    public function tearDown() {
+        if (\file_exists($this->readOnlyLog)) {
+            \unlink($this->readOnlyLog);
+        }
+
+        if (\file_exists($this->writableLog)) {
+            \unlink($this->writableLog);
+        }
+
+        if (\file_exists($this->noTimestampLog)) {
+            \unlink($this->noTimestampLog);
+        }
+    }
+
+}
+
+// End FileLoggerTest
+
+// End libs.sprayfire
