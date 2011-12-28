f0eee735a99a92359610bc7555cb3f28c32d496c
Charles Sprayberry <cspray@gmail.com>
2011-12-28 01:09:12 -0500

    ADD Konfiguration object based off associative array

---
+++ libs/SprayFire/Config/ArrayConfig.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds a class that allows for a configuration to be stored by passing
+ * a simple array
+ *
+ * @details
+ * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
+ * want to make simple, secure, dynamic website content.
+ *
+ * SprayFire repository: http://www.github.com/cspray/SprayFire/
+ *
+ * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
+ *
+ * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ *
+ * @author Charles Sprayberry cspray at gmail dot com
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+namespace SprayFire\Config;
+
+/**
+ * @brief A SprayFire.Config.Configuration object that expects configuration data
+ * to be passed in the form of an array.
+ *
+ * @details
+ * Note that there are no implementing code, everything is handled by SprayFire.Datastructs.ImmutableStoreage
+ */
+class ArrayConfig extends \SprayFire\Datastructs\ImmutableStorage implements \SprayFire\Config\Configuration {
+
+}
