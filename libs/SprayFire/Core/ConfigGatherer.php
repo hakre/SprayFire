05308a4769eccc677b3678628b38526402811926
Charles Sprayberry <cspray@gmail.com>
2011-12-30 12:16:33 -0500

    UPD refactoring the ConfigGatherer

--- libs/SprayFire/Core/ConfigGatherer.php
+++ libs/SprayFire/Core/ConfigGatherer.php
@@ -30,7 +30,41 @@ namespace SprayFire\Core;
 class ConfigGatherer {
 
     /**
-     * @brief The values set for the primary configuration object if the file does
+     * @brief A SprayFire.Config.Configuration object that stores the primary
+     * configuration used by SprayFire.
+     *
+     * @property $PrimaryConfig
+     * @see https://github.com/cspray/SprayFire/wiki/Configuration
+     */
+    protected $PrimaryConfig;
+
+    /**
+     * @brief A SprayFire.Config.Configuration object that stores the routes configuration
+     * used to map a SprayFireURI Pattern to a specific controller and action.
+     *
+     * @property $RoutesConfig
+     *
+     */
+    protected $RoutesConfig;
+
+    /**
+     * @brief The absolute path to the primary configuration file; should be passed
+     * in the constructor as there should be just one \a $PrimaryConfig
+     *
+     * @property $primaryConfigPath
+     */
+    protected $primaryConfigPath;
+
+    /**
+     * @brief The absolute path to the routes configuration file; should be passed
+     * in the constructor as there should be just one \a $RoutesConfig
+     *
+     * @property $routesConfigPath
+     */
+    protected $routesConfigPath;
+
+    /**
+     * @brief Configuration used  for the PrimaryConfig object if the file does
      * not exist in its expected location.
      *
      * @property $defaultPrimaryConfiguration
@@ -46,8 +80,8 @@ class ConfigGatherer {
     );
 
     /**
-     * @brief The value set for the routes configuration object if the file does
-     * not exst in its expected location
+     * @brief Configuration used for the RoutesConfig object if the file does not
+     * exst in its expected location.
      *
      * @property $defaultRoutesConfiguration
      */
@@ -59,32 +93,46 @@ class ConfigGatherer {
     );
 
     /**
-     * @param $primaryConfigPath Complete path to the primary config file
-     * @return SprayFire.Config.Configuration
+     * @param $primaryConfigPath Absolute path to the primary configuration file
+     * @param $routesConfigPath Absolute path to the routes configuration file
      */
-    public function fetchPrimaryConfiguration($primaryConfigPath) {
-        $File = new \SplFileInfo(\SprayFire\Core\Directory::getLibsPath($primaryConfigPath));
-        try {
-            $Config = new \SprayFire\Config\JsonConfig($File);
-        } catch (\InvalidArgumentException $InvalArgExc) {
-            $Config = new \SprayFire\Config\ArrayConfig($this->defaultPrimaryConfiguration);
-        }
-        return $Config;
+    public function __construct($primaryConfigPath, $routesConfigPath) {
+        $this->primaryConfigPath = $primaryConfigPath;
+        $this->routesConfigPath = $routesConfigPath;
     }
 
     /**
-     *
+     * @param $primaryConfigPath Complete path to the primary config file
+     * @return SprayFire.Config.Configuration
+     */
+    public function fetchPrimaryConfiguration() {
+        if (!isset($this->PrimaryConfig)) {
+            $File = new \SplFileInfo(\SprayFire\Core\Directory::getLibsPath($this->primaryConfigPath));
+            try {
+                $this->PrimaryConfig = new \SprayFire\Config\JsonConfig($File);
+            } catch (\InvalidArgumentException $InvalArgExc) {
+                $this->PrimaryConfig = new \SprayFire\Config\ArrayConfig($this->defaultPrimaryConfiguration);
+            }
+        }
+
+        return $this->PrimaryConfig;
+    }
+
+    /**
      * @param $routesConfigPath Complete path to the routes config file
      * @return SprayFire.Config.Configuration
      */
-    public function fetchRoutesConfiguration($routesConfigPath) {
-        $File = new \SplFileInfo(\SprayFire\Core\Directory::getLibsPath($routesConfigPath));
-        try {
-            $Config = new \SprayFire\Config\JsonConfig($File);
-        } catch (\InvalidArgumentException $InvalArgExc) {
-            $Config = new \SprayFire\Config\ArrayConfig($this->defaultRoutesConfiguration);
+    public function fetchRoutesConfiguration() {
+        if (!isset($this->RoutesConfig)) {
+            $File = new \SplFileInfo(\SprayFire\Core\Directory::getLibsPath($this->routesConfigPath));
+            try {
+                $this->RoutesConfig = new \SprayFire\Config\JsonConfig($File);
+            } catch (\InvalidArgumentException $InvalArgExc) {
+                $this->RoutesConfig = new \SprayFire\Config\ArrayConfig($this->defaultRoutesConfiguration);
+            }
         }
-        return $Config;
+
+        return $this->RoutesConfig;
     }
 
 }
