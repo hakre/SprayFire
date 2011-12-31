e943d62b7248b349b10d02b8618aee377fd7f3d0
Charles Sprayberry <cspray@gmail.com>
2011-12-30 21:29:22 -0500

    UPD fixing some docs and adding a new method to ClassLoader to retrieve the list of registered namespaces

--- libs/SprayFire/Core/ClassLoader.php
+++ libs/SprayFire/Core/ClassLoader.php
@@ -42,47 +42,11 @@ class ClassLoader {
     protected $namespaceMap = array();
 
     /**
-     * @brief Allows access to set a given namespace to a given directory.
-     *
-     * @details
-     * The directory should be assigned in such a way that the following occurs:
-     *
-     * <pre>
-     * The class:
-     *
-     * Top.Level.ClassName
-     *
-     * The directory for that class:
-     *
-     * /install_path/app/Top/Level/ClassName.php
-     *
-     * The proper key and value for this namspace and directory would look like:
-     *
-     * $namespaceMap['Top'] = '/install_path/app';
-     *
-     * Thus when you attempt to instantiate the class like so:
-     *
-     * $Class = new Top.Level.ClassName();
-     *
-     * The class autoloader will convert the namespace to a directory and then
-     * append that directory to the value stored by the 'Top' key.
-     * </pre>
-     *
-     * @param $topLevelNamespace A string representing a top level namespace
-     * @param $dir The complete path to the directory holding the top level namespace
-     */
-    public function registerNamespaceDirectory($topLevelNamespace, $dir) {
-        if (!empty($topLevelNamespace) && !empty($dir)) {
-            $this->namespaceMap[$topLevelNamespace] = $dir;
-        }
-    }
-
-    /**
      * @brief Include the class based on the fully namespaced \a $className passed.
      *
      * @param $className The namespaced class to load
      */
-    public function loadClass($className) {
+    public function load($className) {
         $namespace = $this->getTopLevelNamespace($className);
         $path = $this->getDirectoryForTopLevelNamespace($namespace) . '/';
         if (!$path) {
@@ -135,6 +99,49 @@ class ClassLoader {
         $path = \str_replace('\\', '/', $className);
         $path .= '.php';
         return $path;
+    }
+
+    /**
+     * @brief Allows access to set a given namespace to a given directory.
+     *
+     * @details
+     * The directory should be assigned in such a way that the following occurs:
+     *
+     * <pre>
+     * The class:
+     *
+     * Top.Level.ClassName
+     *
+     * The directory for that class:
+     *
+     * /install_path/app/Top/Level/ClassName.php
+     *
+     * The proper key and value for this namspace and directory would look like:
+     *
+     * $namespaceMap['Top'] = '/install_path/app';
+     *
+     * Thus when you attempt to instantiate the class like so:
+     *
+     * $Class = new Top.Level.ClassName();
+     *
+     * The class autoloader will convert the namespace to a directory and then
+     * append that directory to the value stored by the 'Top' key.
+     * </pre>
+     *
+     * @param $topLevelNamespace A string representing a top level namespace
+     * @param $dir The complete path to the directory holding the top level namespace
+     */
+    public function registerNamespaceDirectory($topLevelNamespace, $dir) {
+        if (!empty($topLevelNamespace) && !empty($dir)) {
+            $this->namespaceMap[$topLevelNamespace] = $dir;
+        }
+    }
+
+    /**
+     * @return An array of registered top level namespaces
+     */
+    public function getRegisteredNamespaces() {
+        return \array_keys($this->namespaceMap);
     }
 
 }
