ec6c52aea2a5f2e7005623800840694ecacedb5d
Charles Sprayberry <cspray@gmail.com>
2011-12-30 21:11:14 -0500

    UPD changing test bootstrap to only supply a constant to the installation directory

--- tests/test_bootstrap.php
+++ tests/test_bootstrap.php
@@ -1,12 +1,5 @@
 <?php
 
-    defined('SPRAYFIRE_ROOT') or define('SPRAYFIRE_ROOT', \dirname(__DIR__));
-
-    include \SPRAYFIRE_ROOT .'/libs/SprayFire/Core/Directory.php';
-    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/ClassLoader.php';
-
-    \SprayFire\Core\Directory::setLibsPath(\SPRAYFIRE_ROOT . '/libs');
-    $ClassLoader = new \SprayFire\Core\ClassLoader();
-    $ClassLoader->registerNamespaceDirectory('SprayFire', \SprayFire\Core\Directory::getLibsPath());
-    \spl_autoload_register(array($ClassLoader, 'loadClass'));
-    \SprayFire\Core\Directory::setLibsPath(null);
+// This is a convenience mechanism for testing to get the directory that libs, app
+// and web directory is stored in
+defined('SPRAYFIRE_ROOT') or define('SPRAYFIRE_ROOT', \dirname(__DIR__));
