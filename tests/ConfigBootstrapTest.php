763da70f2b948fdef2eb3e32c0d1aa420f0e64e0
Charles Sprayberry <cspray@gmail.com>
2011-12-31 19:18:30 -0500

    ADD tests/ConfigBootstrapTest initial skeleton

---
+++ tests/ConfigBootstrapTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ *
+ * @details
+ * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
+ * want to make simple, secure, dynamic website content.
+ *
+ * SprayFire repository: http://www.github.com/cspray/SprayFire/
+ *
+ * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
+ *
+ * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ *
+ * @author Charles Sprayberry cspray at gmail dot com
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+
+
+/**
+ * @brief
+ */
+class ConfigBootstrapTest extends PHPUnit_Framework_TestCase {
+
+
+
+}
