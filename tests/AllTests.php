2c61c7d55c5d6374ad003a8f98d03c49eda5f494
Charles Sprayberry <cspray@gmail.com>
2012-01-01 01:44:45 -0500

    UPD ConfigBootstrapTest finishing up first test

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -22,6 +22,7 @@ include 'FileLoggerTest.php';
 include 'DevelopmentLoggerTest.php';
 include 'ClassLoaderTest.php';
 include 'PathGeneratorBootstrapTest.php';
+include 'ConfigBootstrapTest.php';
 
 /**
  * @codeCoverageIgnore
@@ -47,6 +48,7 @@ class AllTests extends PHPUnit_Framework_TestSuite {
         $Suite->addTestSuite('DevelopmentLoggerTest');
         $Suite->addTestSuite('ClassLoaderTest');
         $Suite->addTestSuite('PathGeneratorBootstrapTest');
+        $Suite->addTestSuite('ConfigBootstrapTest');
 
         return $Suite;
     }
--- tests/ConfigBootstrapTest.php
+++ tests/ConfigBootstrapTest.php
@@ -21,6 +21,71 @@
  * @copyright Copyright (c) 2011, Charles Sprayberry
  */
 
+if (!interface_exists('\\SprayFire\\Core\\Object')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Object.php';
+}
+
+
+if (!interface_exists('\\SprayFire\\Core\\Structure\\ObjectMap')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Structure/ObjectMap.php';
+}
+
+if (!interface_exists('\\SprayFire\\Bootstrap\\Bootstrapper')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Bootstrap/Bootstrapper.php';
+}
+
+if (!interface_exists('\\SprayFire\\Core\\Structure\\Overloadable')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Structure/Overloadable.php';
+}
+
+if (!interface_exists('\\SprayFire\\Config\\Configuration')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Config/Configuration.php';
+}
+
+if (!class_exists('\\SprayFire\\Core\\CoreObject')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/CoreObject.php';
+}
+
+if (!class_exists('\\SprayFire\\Logger\\CoreObject')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Logger/CoreObject.php';
+}
+
+if (!class_exists('\\SprayFire\\Bootstrap\\ConfigBootstrap')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Bootstrap/ConfigBootstrap.php';
+}
+
+
+if (!class_exists('\\SprayFire\\Core\\Structure\\DataStorage')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Structure/DataStorage.php';
+}
+
+if (!class_exists('\\SprayFire\\Core\\Structure\\ImmutableStorage')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Structure/ImmutableStorage.php';
+}
+
+if (!class_exists('\\SprayFire\\Core\\Structure\\GenericMap')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Structure/GenericMap.php';
+}
+
+if (!class_exists('\\SprayFire\\Core\\Structure\\RestrictedMap')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Core/Structure/RestrictedMap.php';
+}
+
+if (!class_exists('\\SprayFire\\Config\\ArrayConfig')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Config/ArrayConfig.php';
+}
+
+if (!class_exists('\\SprayFire\\Config\\JsonConfig')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Config/JsonConfig.php';
+}
+
+if (!interface_exists('\\SprayFire\\Logger\\Log')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Logger/Log.php';
+}
+
+if (!class_exists('\\SprayFire\\Logger\\DevelopmentLogger')) {
+    include \SPRAYFIRE_ROOT . '/libs/SprayFire/Logger/DevelopmentLogger.php';
+}
 
 
 /**
@@ -28,6 +93,39 @@
  */
 class ConfigBootstrapTest extends PHPUnit_Framework_TestCase {
 
+    public function testConfigBootstrap() {
 
+        $configPath = \SPRAYFIRE_ROOT . '/tests/mockframework/app/TestApp/Config/json/test-config.json';
+
+        $configs = array(
+            array(
+                'config-object' => '\\SprayFire\\Config\\ArrayConfig',
+                'config-data' => array('sprayfire' => 'best', 'roll' => 'tide'),
+                'map-key' => 'SprayFireRollTide'
+            ),
+            array(
+                'config-object' => '\\SprayFire\\Config\\JsonConfig',
+                'config-data' => $configPath,
+                'map-key' => 'PrimaryConfig'
+            )
+        );
+
+        $Log = new \SprayFire\Logger\DevelopmentLogger();
+        $Bootstrap = new \SprayFire\Bootstrap\ConfigBootstrap($Log, $configs);
+        $Bootstrap->runBootstrap();
+        $ConfigMap = $Bootstrap->getConfigs();
+
+        $SprayFireRollTide = $ConfigMap->getObject('SprayFireRollTide');
+        $PrimaryConfig = $ConfigMap->getObject('PrimaryConfig');
+
+        $this->assertTrue($ConfigMap instanceof \SprayFire\Core\Structure\RestrictedMap);
+        $this->assertTrue($SprayFireRollTide instanceof \SprayFire\Config\ArrayConfig);
+        $this->assertTrue($PrimaryConfig instanceof \SprayFire\Config\JsonConfig);
+
+        $this->assertSame('best', $SprayFireRollTide->sprayfire);
+        $this->assertSame('tide', $SprayFireRollTide->roll);
+        $this->assertSame('Roll Tide!', $PrimaryConfig->app->{'deep-one'}->{'deep-two'}->{'deep-three'}->value);
+
+    }
 
 }
