0884c702036a8e32d3812c2c672e8f100ba6a3bc
Charles Sprayberry <cspray@gmail.com>
2012-01-01 02:58:26 -0500

    UPD HttpData bringing docs up to speed

--- libs/SprayFire/Request/HttpData.php
+++ libs/SprayFire/Request/HttpData.php
@@ -29,8 +29,20 @@ namespace SprayFire\Request;
  * @brief Will allow for an array to be passed, the values of that array to be
  * treated as an object, and for changes to the array or the object to make
  * changes to the other.
+ *
+ * @details
+ * Designed to provide a base for objects needing to work with PHP superglobals
+ * and perform methods or validation on that data, as an example.
+ *
+ * It is important to realize that due to the implementation of this object and
+ * that of SprayFire.Core.Strcuture.MutableStorage it is not possible to convert
+ * arrays in data passed into MutableStorage objects.  You will have to implement
+ * this yourself if you would like this functionality.  Ultimately, I would not do
+ * this however, as the array can ultimately be changed outside of this class.
+ *
+ * @uses SprayFire.Core.Structure.MutableStorage
  */
-abstract class HttpData extends \SprayFire\Datastructs\MutableStorage {
+abstract class HttpData extends \SprayFire\Core\Structure\MutableStorage {
 
     /**
      * @param $data An array passed by reference
